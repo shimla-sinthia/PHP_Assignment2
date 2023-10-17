29624e860d1c8fe7cd7bd1ad180511dbcbe0173d
shimla-sinthia <67505798+shimla-sinthia@users.noreply.github.com>
2023-10-17 13:43:35 +0600

    Update index.php

--- index.php
+++ index.php
@@ -23,49 +23,50 @@ $book3 = new Book(
     0
 );
 
-$customer1 = new  Customer(
+$customer1 = new Customer(
     1,
     'Adrit',
     'Chowdhury',
     'adrit.chowdhury@gmail.com'
 );
-$customer2 = new  Customer(
+$customer2 = new Customer(
     2,
     'Ayushman',
     'Chowdhury',
     'ayushman.chowdhury@gmail.com'
 );
 
+
 echo "<b>BOOK</b></br>";
 // isAvailable
-echo "Book-1 is available? " . $book1->__call("isAvailable","") . "</br>";
+echo "Book-1 is available? " . $book1->isAvailable() . "</br>";
 // getPrintableTitle
-echo "Book-1 Title: " . $book1->__call("getPrintableTitle","") . "</br>";
+echo "Book-1 Title: " . $book1->getPrintableTitle() . "</br>";
 
 echo "</br>";
 
 // addCopy
-echo "Book-1 Title: " . $book1->__call("getPrintableTitle","") . "</br>";
-echo "Book-3 availableCopy: " . $book3->__get("availableCopy") . "</br>"; 
-$book3->__call("addCopy","2"); // add A copy of Book-3
-echo "Book-3 availableCopy after adding 2 copy: " . $book3->__get("availableCopy") . "</br>";
+echo "Book-1 Title: " . $book1->getPrintableTitle() . "</br>";
+echo "Book-3 availableCopy: " . $book3->availableCopy . "</br>";
+$book3->__call("addCopy", "2"); // add A copy of Book-3
+echo "Book-3 availableCopy after adding 2 copy: " . $book3->availableCopy . "</br>";
 
 echo "</br>";
 
 //getTitle
-echo "Book-1 Title: <b>" . $book1->__get("title") . "</b></br>";
+echo "Book-1 Title: <b>" . $book1->title . "</b></br>";
 //getAuthor
-echo "Book-1 Author: <b>" . $book1->__get("author") . "</b></br>";
+echo "Book-1 Author: <b>" . $book1->author . "</b></br>";
 //getIsbn
-echo "Book-1 Isbn: <b>" . $book1->__get("isbn") . "</b></br>";
+echo "Book-1 Isbn: <b>" . $book1->isbn . "</b></br>";
 
 //Customer INFO.
 echo "</br> <b>CUSTOMER</b> </br>";
-echo "Customer-1 Id: <b>" . $customer1->__get("id") . "</b></br>";
-echo "Customer-1 First Name: <b>" . $customer1->__get("firstName") . "</b></br>";
-echo "Customer-1 Last Name: <b>" . $customer1->__get("lastName") . "</b></br>";
-echo "Customer-1 Email: <b>" . $customer1->__get("email") . "</b></br>";
-$customer1->__set("email","adrit@gmail.com");
-echo "Customer-1 Email: <b>" . $customer1->__get("email") . "</b></br>";
+echo "Customer-1 Id: <b>" . $customer1->id . "</b></br>";
+echo "Customer-1 First Name: <b>" . $customer1->firstName . "</b></br>";
+echo "Customer-1 Last Name: <b>" . $customer1->lastName . "</b></br>";
+echo "Customer-1 Email: <b>" . $customer1->email . "</b></br>";
+$customer1->email = "adrit@gmail.com";
+echo "Customer-1 Email after update: <b>" . $customer1->email . "</b></br>";
 
 ?>
